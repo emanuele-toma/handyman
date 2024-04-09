85360999ac7af88e22821d1a37c5eaffe3643ecc
emanuele-toma <toma.emanuele03@gmail.com>
2024-04-09 13:44:03 +0200

    👷 Add CI for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: '/handyman',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
